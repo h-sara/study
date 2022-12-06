b68a4ace5a0dad36c483aa85ff1d9328d1a75153
h-sara <115357536+h-sara@users.noreply.github.com>
2022-12-06 06:43:55 +0000

    [Finish] JavaScriptを学ぼう_chapter2

--- javascript/chapter_1/js/app.js
+++ javascript/chapter_1/js/app.js
@@ -1,10 +1,1 @@
-// alert("Hello World");
-
-// let hello = "Hello World";
-// alert(hello);
-
-// alert("Hello" + "World");
-
-let str1 = "Hello";
-let str2 = "World";
-alert(str1 + str2);
+alert("Hello World");
---
+++ javascript/chapter_2/js/app.js
@@ -0,0 +1,55 @@
+// let hello = "Hello World";
+// alert(hello);
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+
+// let str1 = "Hello";
+// let str2 = "World";
+// alert(str1 + str2);
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert("みかんの値段がりんごより安い");
+// } else if(orange == apple){
+//   alert("みかんとりんごが同じ値段");
+// } else {
+//   alert("みかんの値段がりんごより高い");
+// }
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert("2をかけ続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+
+// let i;
+// let num = 0;
+
+// for(i =1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert("1から10まで足し算した結果は" + num + "です");
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+// 確認問題
+let num1 = 1;
+let num2 = 2;
+
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 / num2);
+alert(num1 * num2);
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
