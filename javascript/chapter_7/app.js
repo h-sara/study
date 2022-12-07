5919bef3ec213a65469d6ec6088c135d51f5657c
h-sara <115357536+h-sara@users.noreply.github.com>
2022-12-07 07:26:02 +0000

    [Start] JavaScriptを学ぼう_chapte7

---
+++ javascript/chapter_7/app.js
@@ -0,0 +1,53 @@
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+// });
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+
+$(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+
+
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+
+
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
+
+
+
+/*－－－－－－－－－－－－－－－－－－－ー－－－－－－－－－－－－－－－－－－－－*/
